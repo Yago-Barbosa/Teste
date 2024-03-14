22f59d9f73fdcded8143c8401d4138af89059803
Yago-Barbosa <yagosbarbosa26@gmail.com>
2024-03-14 16:09:41 -0300

    primeiros passos

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// Divisão lógica do programa, pode ser reutilizado.
+namespace Teste;
+// Recomendado ter apenas uma classe e namespace por aquivos.
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var texto = "testando";
+        Console.WriteLine(texto);
+    }
+}
